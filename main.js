af7ef78cf26bdd28dbe5e12b6d68a0bf103902ea
Leonardo Oliveira <60435551+leonardovoliveira@users.noreply.github.com>
2023-04-26 11:42:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,16 +4,12 @@ function tocaSom(id){
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
-let i = 0;
-
-while(i < listaTeclas.length) {
-    
+for (let i= 0; i < listaTeclas.length; i++) {
     const tecla = listaTeclas[i];
     const instrumento = tecla.classList[1];
     const id = `#som_${instrumento}`;
-
+    
     tecla.onclick = function (){
         tocaSom(id);
     }
-    i++;
 }
