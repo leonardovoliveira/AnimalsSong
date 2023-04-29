a8cad0023a82ea912d8b56b89d524decaaeca91b
Leonardo Oliveira <60435551+leonardovoliveira@users.noreply.github.com>
2023-04-29 19:41:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,13 @@ for (let i= 0; i < listaTeclas.length; i++) {
     tecla.onclick = function (){
         tocaSom(id);
     }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === "Enter" || evento.code === "Space"){
+            tecla.classList.add('ativa');
+            tecla.onkeyup = function(){
+                tecla.classList.remove('ativa');
+        }
+    }
+}
 }
